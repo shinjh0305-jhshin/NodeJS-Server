ac0a3310a038c88127049b8d3a4b58a44493215b
shinjh0305 <tlswogus30@naver.com>
2022-06-15 17:02:39 +0900

    cookie&auth 완성

--- Cookie_Auth/main.js
+++ Cookie_Auth/main.js
@@ -5,6 +5,27 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var cookie = require('cookie');
+
+function authIsOwner(request, response) {
+  var isOwner = false;
+  var cookies = {};
+  if (request.headers.cookie) {
+    cookies = cookie.parse(request.headers.cookie);
+  }
+  if (cookies.email === 'abcd' && cookies.password === '111111') {
+    isOwner = true;
+  }
+  return isOwner;
+}
+
+function authStatusUI(request, response) {
+  var authStatusUI = `<a href="/login">login</a>`;
+  if (authIsOwner(request, response)) {
+    authStatusUI = `<a href="/logout_process">logout</a>`;
+  }
+  return authStatusUI;
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -18,7 +39,8 @@ var app = http.createServer(function(request,response){
           var list = template.list(filelist);
           var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
+            `<a href="/create">create</a>`,
+            authStatusUI(request, response)
           );
           response.writeHead(200);
           response.end(html);
@@ -40,7 +62,8 @@ var app = http.createServer(function(request,response){
                 <form action="delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
-                </form>`
+                </form>` ,
+                authStatusUI(request, response)
             );
             response.writeHead(200);
             response.end(html);
@@ -48,6 +71,10 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
+      if (authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = template.list(filelist);
@@ -61,11 +88,15 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
+        `, '', authStatusUI(request, response));
         response.writeHead(200);
         response.end(html);
       });
     } else if(pathname === '/create_process'){
+      if (authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -80,6 +111,10 @@ var app = http.createServer(function(request,response){
           })
       });
     } else if(pathname === '/update'){
+      if (authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
       fs.readdir('./data', function(error, filelist){
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
@@ -98,19 +133,18 @@ var app = http.createServer(function(request,response){
               </p>
             </form>
             `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+            authStatusUI(request, response)
           );
           response.writeHead(200);
           response.end(html);
         });
       });
-    } else if (1===1){
-
-      //9.3부터 재개.
-
-
-
     } else if(pathname === '/update_process'){
+      if (authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -128,6 +162,10 @@ var app = http.createServer(function(request,response){
           });
       });
     } else if(pathname === '/delete_process'){
+      if (authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -151,14 +189,53 @@ var app = http.createServer(function(request,response){
             <p><input type="password" name="password" placeholder="password"></p>
             <p><input type="submit"></p>
           </form>`,
-          `<a href="/create">create</a>`
+          `<a href="/create">create</a>`,
         );
         response.writeHead(200);
         response.end(html);
       });
+    } else if (pathname === '/login_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          if (post.email === 'abcd' && post.password === '111111'){
+            response.writeHead(302, {
+              'Set-Cookie' : [
+                `email=${post.email}`,
+                `password=${post.password}`,
+                `nickname=Jaehyun Shin`
+              ],
+              Location: `/`
+            });
+            response.end();
+          } else {
+            response.end('Who?');
+          }
+      });
+    } else if (pathname === '/logout_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+
+          response.writeHead(302, {
+            'Set-Cookie' : [
+              `email=; Max-Age=0`,
+              `password=; Max-Age=0`,
+              `nickname=; Max-Age=0`
+            ],
+            Location: `/`
+          });
+          response.end();
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
-    }
+   }
 });
 app.listen(3000);
