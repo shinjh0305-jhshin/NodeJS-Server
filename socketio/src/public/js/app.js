97c2de01984ddb0c84f00f5eeead07893717bcfc
Jaehyun Shin_Linux <tlswogus30@naver.com>
2022-08-01 20:34:43 +0900

    basic socketio

--- socketio/src/public/js/app.js
+++ socketio/src/public/js/app.js
@@ -1,43 +1,26 @@
 //웹소켓의 frontend
 
-const messageList = document.querySelector('ul');
-const nickForm = document.querySelector('#nick');
-const messageForm = document.querySelector('#message');
-const socket = new WebSocket(`ws://${window.location.host}`); //window.location.host : 프런트에서 접속한 주소
+const socket = io();
+const welcome = document.querySelector('#welcome');
+const room = document.getElementById('room');
+const form = welcome.querySelector('form');
+let roomName;
 
-socket.addEventListener("open", () => { //서버와 연결되었을 때
-    console.log("Connected to server");
-})
+room.hidden = true;
 
-socket.addEventListener("message", (message) => { //서버로부터 메시지를 전달받았을 때
-    const li = document.createElement('li');
-    li.innerText = message.data;
-    messageList.append(li);
-    //console.log(message);
-})
-
-socket.addEventListener("close", () => { //서버와 연결이 종료되었을 때
-    console.log("Disconnected from Server");
-})
-
-function makeMessage(type, payload) {
-    const msg = { type, payload };
-    return JSON.stringify(msg);
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector('h3');
+    h3.innerText = `Room ${roomName}`
 }
 
-function handleSubmit(event) {
-    event.preventDefault(); 
-    const input = messageForm.querySelector('input');
-    socket.send(makeMessage("new_message", input.value));
-    input.value = "";
-}
-
-function handleNickSubmit(event) {
+function handleRoomSubmit(event) {
     event.preventDefault();
-    const input = nickForm.querySelector('input');
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
+    const input = form.querySelector('input');
+    socket.emit("enter_room", input.value, showRoom); //emits event(Event name : enter_room), param #3 : 백엔드에서 호출 가능한 함수를 콜백으로 전달해준다.
+    roomName = input.value;
+    input.value = '';
 }
 
-nickForm.addEventListener("submit", handleNickSubmit);
-messageForm.addEventListener("submit", handleSubmit);
+form.addEventListener('submit', handleRoomSubmit);
--- socketio/src/server.js
+++ socketio/src/server.js
@@ -2,9 +2,8 @@
 
 const express = require('express');
 const http = require('http');
-const Websocket = require('ws');
+const SocketIO = require('socket.io');
 const morgan = require('morgan');
-const { json } = require('express');
 const app = express();
 
 app.set("port", 3000 || process.env.PORT);
@@ -20,35 +19,21 @@ app.get('/', (req, res) => {
 })
 
 //http와 websocket 모두 사용하기 위해서 websocket에 http 서버를 전달해줬다.
-const server = http.createServer(app);
-const wss = new Websocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = []; //fake database
-
-wss.on("connection", (socket) => {
-    sockets.push(socket); //sockets에 socket 객체(접속 정보)를 저장한다.
-    socket.nickname = "Anonymous";
-    console.log("Connected to browser");
-    //socket.send("Hello!!");
-    socket.on("close", () => {
-        console.log("Disconnected from Browser");
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (roomname, changeVisibility) =>{ //changeVisibility : 프런트에서 전달받은 콜백함수를 프런트에서 실행한다.
+        changeVisibility();
+        console.log(roomname); //sogang(채팅방 이름)
+        console.log(socket.id); //0l8Ik2Vp_m7KxlS8AAAB(primary key of the socket)
+        console.log(socket.rooms); //Set(1) { '0l8Ik2Vp_m7KxlS8AAAB' } (sogang에 참여하기 전에는 '나' 채팅방 존재)
+        socket.join(roomname); //sogang에 입장
+        console.log(socket.rooms); //Set(2) { '0l8Ik2Vp_m7KxlS8AAAB', 'sogang' } ('나'와 'sogang'모두 존재)
     });
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type) {
-            case "nickname" :
-                socket.nickname = message.payload; //소켓에 nickname이라는 키를 추가한다.
-                break;
-            case "new_message" :
-                sockets.forEach(item => item.send(`${socket.nickname}: ${message.payload}`));
-                break;
-        }
-    })
-});
+})
 
-server.listen(app.get("port"), () => {
+
+httpServer.listen(app.get("port"), () => {
     console.log(`Server running on ${app.get("port")}`);
 })
-// app.listen(app.get("port"), () => {
-//     console.log(`Server running on ${app.get("port")}`);
-// })
